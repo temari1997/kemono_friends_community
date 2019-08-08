271d257f958df5eee53c7e0d754081d018105ca2
kurakore(KyotoUniv) <kurahara.koreharu.85m@st.kyoto-u.ac.jp>
2019-08-09 08:29:07 +0900

    避難

---
+++ get-list-tweets.py
@@ -0,0 +1,50 @@
+import json
+from requests_oauthlib import OAuth1Session
+import pandas as pd
+import datetime
+import time
+
+
+# In[2]:
+def authentication():
+    from requests_oauthlib import OAuth1Session
+    import csv
+
+    input_file = open('auth.csv', 'r', encoding="utf-8")
+    reader = csv.DictReader(input_file)
+    keys = [row for row in reader]
+    input_file.close()
+
+    return OAuth1Session(keys[0]["API_key"], keys[0]["API_secret"], keys[0]["Access_token"], keys[0]["Access_secret"])
+
+def getTweets(lastId):
+    url = "https://api.twitter.com/1.1/lists/statuses.json"
+
+    if lastId == 0:
+        params = {'list_id':1144937680683147267, 'count':200}
+    else:
+        params = {'list_id':1144937680683147267, 'count':200, 'since_id':lastId}
+    return twitter.get(url, params=params)
+    
+    
+def saveTweets(tweets):
+    filename = 'json/tweets' + datetime.datetime.today().strftime('%m%d-%H%M') + '.json'
+    f = open(filename, 'w', encoding='utf-8')
+    json.dump(json.loads(tweets.text), f, ensure_ascii=False, indent=2)
+    print(len(json.loads(tweets.text)))
+    f.close()
+
+def work(lastId):
+    tweets = getTweets(lastId)
+    nextLastId = json.loads(tweets.text)[0]['id']
+    saveTweets(tweets)
+    return nextLastId
+
+#初期化
+twitter = authentication()
+lastId = 0
+while True:
+    nextId = work(lastId)
+    lastId = nextId
+    print(datetime.datetime.now())
+    time.sleep(600)
